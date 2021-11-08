848bef39cf50ecb731d1c659569baadeaa5fd2e1
Jonas Balin <jonasbalin@gmail.com>
2021-11-08 20:47:56 +0100

    Added table component

--- scancar/src/App.js
+++ scancar/src/App.js
@@ -1,11 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import BasicTable from "./components/table.tsx";
 
 function App() {
   const data = [
-    { BookingID: "146", Name: "video 1 text", PhoneNum: "10304050", Group: "A", Pickup: "01/11/2010", return: "20/11/2010", status: "picked up" },
-    { BookingID: "147", Name: "video 2 text", PhoneNum: "10304050", Group: "A", Pickup: "01/11/2010", return: "20/11/2010", status: "picked up" },
-    { BookingID: "148", Name: "video 3 text", PhoneNum: "10304050", Group: "A", Pickup: "01/11/2010", return: "20/11/2010", status: "picked up" },
+    {
+      BookingID: "146",
+      Name: "video 1 text",
+      PhoneNum: "10304050",
+      Group: "A",
+      Pickup: "01/11/2010",
+      return: "20/11/2010",
+      status: "picked up",
+    },
+    {
+      BookingID: "147",
+      Name: "video 2 text",
+      PhoneNum: "10304050",
+      Group: "A",
+      Pickup: "01/11/2010",
+      return: "20/11/2010",
+      status: "picked up",
+    },
+    {
+      BookingID: "148",
+      Name: "video 3 text",
+      PhoneNum: "10304050",
+      Group: "A",
+      Pickup: "01/11/2010",
+      return: "20/11/2010",
+      status: "picked up",
+    },
   ];
 
   return (
@@ -25,50 +50,51 @@ function App() {
               <form>
                 <label>
                   Booking ID
-                  <input type="text" name="Booking ID" />
                 </label>
+                  <input type="text" name="Booking ID" />
                 <label>
                   Phone Number
-                  <input type="text" name="Phone Number" />
                 </label>
+                  <input type="text" name="Phone Number" />
                 <label>
                   Name
+                  </label>
                   <input type="text" name="Name" />
-                </label>
                 <label>
                   Address
+                  </label>
                   <input type="text" name="Address" />
-                </label>
               </form>
             </div>
             <button id="advfilters">Advanced Filters</button>
-
           </div>
           <div class="module" id="advfiltersmodule">
-          <form>
-                <label>
-                  License Plate
-                  <input type="text" name="License Plate" />
-                </label>
-                <label>
-                  Customer ID
-                  <input type="text" name="Customer ID" />
-                </label>
-                <label>
-                  Driver's License
-                  <input type="text" name="Driver's License" />
-                </label>
-                </form>
-                <input type="checkbox" id="checkboxA"></input> <label>A</label>
-                <input type="checkbox" id="checkboxB"></input> <label>B</label>
-                <input type="checkbox" id="checkboxC"></input> <label>C</label>
-                <br></br>
-                <input type="checkbox" id="checkboxD"></input> <label>D</label>
-                <input type="checkbox" id="checkboxE"></input> <label>E</label>
-                <input type="checkbox" id="checkboxF"></input> <label>F</label>
+            <form>
+              <label>
+                License Plate
+                <input type="text" name="License Plate" />
+              </label>
+              <label>
+                Customer ID
+                <input type="text" name="Customer ID" />
+              </label>
+              <label>
+                Driver's License
+                <input type="text" name="Driver's License" />
+              </label>
+            </form>
+            <input type="checkbox" id="checkboxA"></input> <label>A</label>
+            <input type="checkbox" id="checkboxB"></input> <label>B</label>
+            <input type="checkbox" id="checkboxC"></input> <label>C</label>
+            <br></br>
+            <input type="checkbox" id="checkboxD"></input> <label>D</label>
+            <input type="checkbox" id="checkboxE"></input> <label>E</label>
+            <input type="checkbox" id="checkboxF"></input> <label>F</label>
           </div>
           <div class="module" id="listmodule">
             <h3>Lists are to be here</h3>
+
+            <BasicTable/>
 
             <table>
               {data.map((el) => (
