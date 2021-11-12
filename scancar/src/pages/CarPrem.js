6caa728faee47cedf1d4cd5ba5e46941f6a92a7e
Jonas Sylvian Jersild Balin <jobal@semler.dk>
2021-11-12 13:44:09 +0100

    Prelim. work on CarPrem page

--- scancar/src/pages/CarPrem.js
+++ scancar/src/pages/CarPrem.js
@@ -1,19 +1,36 @@
 import * as React from 'react';
 //import logo from "../logo.svg";
-import "../App.css";
+import "./CarPrem.css"
 import BasicTable from "../components/table.tsx";
-import CheckBoxes from "../components/checkboxes.tsx";
-
 
 import { Link } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
-
-
-function CarPrem() {
+function Home() {
+    const [visible, setVisible] = React.useState(false);
 
     return (
-    <h1>This is CarPrem</h1>
+    <div className="Background">
+      <div class="navbar">
+        <h1>ScanCar</h1>
+      </div>
+
+    
+      <div>
+        <div class="canvas">
+          <div class="moduletitle" id="carprem">
+            <h2>Cars on Premises</h2>
+          </div>
+          <div class="module" id="overviewmodule">
+            <h3>Overview</h3>
+            <BasicTable></BasicTable>
+          </div>
+          <div class="module" id="parkingmodule">
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default CarPrem;
+export default Home;
