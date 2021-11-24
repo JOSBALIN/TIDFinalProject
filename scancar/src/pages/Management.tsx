af488f205d225d16d621695611745b16eae1a8e2
Jonas Balin <jonasbalin@gmail.com>
2021-11-24 18:58:53 +0100

    tsx conversion

---
+++ scancar/src/pages/Management.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+//import logo from "../logo.svg";
+import "../App.css";
+import BasicTable from "../components/table";
+import CheckBoxes from "../components/checkboxes";
+
+
+import { Link } from 'react-router-dom';
+
+
+
+function Management() {
+
+    return (
+    <h1>This is Management</h1>
+  );
+}
+
+export default Management;
