7e22f435726baa5a16ceba68277a895756bc8333
Jonas Balin <jonasbalin@gmail.com>
2022-01-04 21:59:38 +0100

    prelim changes to carprem overview

--- scancar/src/components/caroverview.js
+++ scancar/src/components/caroverview.js
@@ -1,237 +1,65 @@
-
+import "./parkingLotSpot.js";
 import "./caroverviewgrid.css";
 import "./CarPrem.css";
- 
-
+import ParkingLotSpot from "./parkingLotSpot.js";
 
 export default function LotOverview(props) {
-        return (
-            <div className="grid-container">
-            <div className="grid-item grid-item-1">    
-                <div className="small-grid-box-container-horizontal">
-                    <div className="small-grid-box" id="c1">
-                        <h3>
-                            c1    
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c2">
-                        <h3>
-                            c2
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c3">
-                        <h3>
-                            c3    
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c4">
-                        <h3>
-                            c4
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c5">
-                        <h3>
-                            c5 
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c6">
-                        <h3>
-                            c6
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c7">
-                        <h3>
-                            c7
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c8">
-                        <h3>
-                            c8
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c9">
-                        <h3>
-                            c9
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c10">
-                        <h3>
-                            c10    
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c11">
-                        <h3>
-                            c11    
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c12">
-                        <h3>
-                            c12   
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c13">
-                        <h3>
-                            c13
-                        </h3>    
-                    </div>                    
-                    <div className="small-grid-box" id="c14">
-                        <h3>
-                            c14
-                        </h3>    
-                    </div>                    
-                </div>
-            </div>
-            <div className="grid-item grid-item-2">
-                <div className="small-grid-box-container-vertical">
-                    <div className="small-grid-box" id="a1">
-                        <h3>
-                            a1
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a2">
-                        <h3>
-                            a2
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a3">
-                        <h3>
-                            a3
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a4">
-                        <h3>
-                            a4
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a5">
-                        <h3>
-                            a5
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a6">
-                        <h3>
-                            a6
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a7">
-                        <h3>
-                            a7
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a8">
-                        <h3>
-                            a8
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a9">
-                        <h3>
-                            a9
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a10">
-                        <h3>
-                            a10
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a11">
-                        <h3>
-                            a11
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a12">
-                        <h3>
-                            a12
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a13">
-                        <h3>
-                            a13
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="a14">
-                        <h3>
-                            a14
-                        </h3>
-                    </div>
-                </div>
-            </div>
-            <div className="grid-item grid-item-3">
-                <div className="small-grid-box-container-vertical">
-                    <div className="small-grid-box" id="b1">
-                        <h3>
-                            b1
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b2">
-                        <h3>
-                            b2
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b3">
-                        <h3>
-                            b3
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b4">
-                        <h3>
-                            b4
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b5">
-                        <h3>
-                            b5
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b6">
-                        <h3>
-                            b6
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b7">
-                        <h3>
-                            b7
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b8">
-                        <h3>
-                            b8
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b9">
-                        <h3>
-                            b9
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b10">
-                        <h3>
-                            b10
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b11">
-                        <h3>
-                            b11
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b12">
-                        <h3>
-                            b12
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b13">
-                        <h3>
-                            b13
-                        </h3>
-                    </div>
-                    <div className="small-grid-box" id="b14">
-                        <h3>
-                            b14
-                        </h3>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="grid-container">
+      <div className="grid-item grid-item-1">
+        <div className="small-grid-box-container-horizontal">
+          <ParkingLotSpot id={"C1"} className={"small-grid-box ready"} />
+          <ParkingLotSpot id={"C2"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C3"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C4"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C5"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C6"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C7"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C8"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C9"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C10"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C11"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C12"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C13"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"C14"} className={"small-grid-box maintenance"} />
         </div>
-        
-            
-            );
-        }
-        
+      </div>
+      <div className="grid-item grid-item-2">
+        <div className="small-grid-box-container-vertical">
+          <ParkingLotSpot id={"A1"} className={"small-grid-box ready"} />
+          <ParkingLotSpot id={"A2"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A3"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A4"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A5"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A6"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A7"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A8"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A9"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A10"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A11"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A12"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A13"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"A14"} className={"small-grid-box maintenance"} />
+        </div>
+      </div>
+      <div className="grid-item grid-item-3">
+        <div className="small-grid-box-container-vertical">
+          <ParkingLotSpot id={"B1"} className={"small-grid-box ready"} />
+          <ParkingLotSpot id={"B2"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B3"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B4"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B5"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B6"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B7"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B8"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B9"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B10"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B11"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B12"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B13"} className={"small-grid-box"} />
+          <ParkingLotSpot id={"B14"} className={"small-grid-box maintenance"} />
+        </div>
+      </div>
+    </div>
+  );
+}
