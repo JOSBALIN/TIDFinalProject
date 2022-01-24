629e379c36f395509dac075568e905af38b844b2
Amalie Pedersen <amap@itu.dk>
2022-01-24 11:36:00 +0100

    made ScanCar clickable

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     return(
       <>
       <div className="navbar">
-          <h1>ScanCar</h1>
+      <a href="/"><h1>ScanCar</h1></a>
       </div>
       </>
     )
